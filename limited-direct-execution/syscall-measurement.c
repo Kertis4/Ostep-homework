de1eb5b2e067058220b432cb60d2baf99a85024a
Kertis4 <eoincur14@gmail.com>
2026-03-24 21:34:57 +0000

    done some sycall measurment stuff (still need to normalise it)

---
+++ limited-direct-execution/syscall-measurement.c
@@ -0,0 +1,36 @@
+#include <bits/types/struct_timeval.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+int main() {
+
+  struct timeval tStart;
+  struct timeval tEnd;
+
+  int fd = open("test.txt", O_RDONLY);
+
+  if (fd < 0) {
+    perror("open");
+    return 1;
+  }
+
+  char buf[64];
+
+  int i = 0;
+
+  int startTime = gettimeofday(&tStart, NULL);
+  while (i < 10000) {
+
+    ssize_t n = read(fd, buf, 0);
+    ++i;
+  }
+
+  int endTime = gettimeofday(&tEnd, NULL);
+
+  int difSec = tEnd.tv_sec - tStart.tv_sec;
+  int difMSec = tEnd.tv_usec - tStart.tv_usec;
+
+  printf("It took %d.%d seconds.microseconds to run 10000 0 byte reads \n",
+         difSec, difMSec);
+}
