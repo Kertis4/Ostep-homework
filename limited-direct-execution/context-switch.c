53948472c405e29eec9fdfa2bb97380d0850c932
Kertis4 <eoincur14@gmail.com>
2026-03-28 22:48:11 +0000

    Context Switch Measured

--- limited-direct-execution/context-switch.c
+++ limited-direct-execution/context-switch.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 #include <sys/time.h>
-
+#include <unistd.h>
 int main() {
+  int p1[2], p2[2];
+  pipe(p1);
+  pipe(p2);
+
   struct timeval tStart;
   struct timeval tEnd;
 
   int i = 0;
   int calls = 10000;
+  char buf = 'x';
+
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    while (i < calls) {
+      read(p1[0], &buf, 1);
+      write(p2[1], &buf, 1);
+
+      ++i;
+    }
+    return 0;
+  }
 
   int startTime = gettimeofday(&tStart, NULL);
   while (i < calls) {
-    // context switch logic goes here
-
+    write(p1[1], &buf, 1);
+    read(p2[0], &buf, 1);
     ++i;
   }
   int endTime = gettimeofday(&tEnd, NULL);
@@ -24,10 +41,10 @@ int main() {
     difMSec += 1000000;
   }
   double TotalMicro = (1000000.0 * difSec) + difMSec;
-  double TimePerCall = TotalMicro / (double)calls;
+  double TimePerCall = TotalMicro / ((double)calls * 2);
 
   printf("It took %f microseconds to make %d context switches \n", TotalMicro,
 
-         calls);
+         (calls * 2));
   printf("That is 1 conext switch every %.4f microseconds \n", TimePerCall);
 }
